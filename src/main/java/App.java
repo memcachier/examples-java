77db2b956681b61c97c66400c5976282201e5ced
David Terei <davidterei@gmail.com>
2013-06-01 13:23:02 -0700

    fix code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,26 +2,34 @@ import java.io.IOException;
 import java.util.Random;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.auth.AuthDescriptor;
+import net.spy.memcached.auth.PlainCallbackHandler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
-import net.spy.memcached.AddrUtil;
-import net.spy.memcached.MemcachedClient;
-import net.spy.memcached.ConnectionFactoryBuilder;
-import net.spy.memcached.auth.PlainCallbackHandler;
-import net.spy.memcached.auth.AuthDescriptor;
 
+/**
+ * MemCachier Java Example Application.
+ *
+ * @author MemcCachier Inc.
+ */
 public class App extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     Random generator = new Random();
-    int digit = generator.nextInt(30);
+    int digit = generator.nextInt(50);
     Result r = findOrComputeDigit(digit);
 
     StringBuilder sb = new StringBuilder();
     sb.append("<h1>MemCachier Fibonacci Example</h1>");
-    sb.append("<p>This script computes a random digit of the Fibonacci sequence.  Before computing, though, it checks to see if there's a cached value for the digit and serves the cached value if there's a hit.</p>");
+    sb.append("<p>This script computes a random digit of the Fibonacci " +
+			 "sequence.  Before computing, though, it checks to see if there's " +
+			 "a cached value for the digit and serves the cached value if " +
+			 "there's a hit.</p>");
     sb.append("<p>Digit: " + digit + "</p>");
     sb.append("<p>Value: " + r.val + "</p>");
     sb.append("<p>Was in cache? " + r.cached + "</p>");
@@ -40,16 +48,27 @@ public class App extends HttpServlet {
     server.join();
   }
 
+  private static class Result {
+    public int val;
+    public boolean cached;
+
+    public Result(int val, boolean cached) {
+      this.val = val;
+      this.cached = cached;
+    }
+  }
   private static Result findOrComputeDigit(int n) {
-    AuthDescriptor ad = new AuthDescriptor(new String[] { "PLAIN" },
-        new PlainCallbackHandler(System.getenv("MEMCACHIER_USERNAME"),
-            System.getenv("MEMCACHIER_PASSWORD")));
+    AuthDescriptor ad = new AuthDescriptor(
+			 new String[] { "PLAIN" },
+			 new PlainCallbackHandler(System.getenv("MEMCACHIER_USERNAME"),
+				                       System.getenv("MEMCACHIER_PASSWORD"))
+			 );
 
     try {
-      MemcachedClient mc = new MemcachedClient(new ConnectionFactoryBuilder()
-          .setProtocol(ConnectionFactoryBuilder.Protocol.BINARY)
-          .setAuthDescriptor(ad).build(), AddrUtil.getAddresses(System
-          .getenv("MEMCACHIER_SERVERS") + ":11211"));
+		MemcachedClient mc = new MemcachedClient(
+				new ConnectionFactoryBuilder().setProtocol(
+					ConnectionFactoryBuilder.Protocol.BINARY).setAuthDescriptor(ad).build(),
+				AddrUtil.getAddresses(System.getenv("MEMCACHIER_SERVERS")));
       Object inCache = mc.get("" + n);
       if (inCache == null) {
         return new Result(computeAndSet(mc, n), false);
@@ -57,9 +76,9 @@ public class App extends HttpServlet {
         return new Result((Integer) inCache, true);
       }
     } catch (IOException ex) {
-      System.err
-          .println("Couldn't create a connection, bailing out: \nIOException "
-              + ex.getMessage());
+      System.err.println(
+				"Couldn't create a connection, bailing out: \nIOException "
+				+ ex.getMessage());
       return new Result(fibDigit(n), false);
     }
   }
@@ -78,13 +97,5 @@ public class App extends HttpServlet {
     }
   }
 
-  private static class Result {
-    public int val;
-    public boolean cached;
-
-    public Result(int val, boolean cached) {
-      this.val = val;
-      this.cached = cached;
-    }
-  }
 }
+
